44a786ae8f8cba7c75a7db82ee995bed9eea693e
jmarchel7bulls <jmarchel@7bulls.com>
2024-11-19 16:26:41 +0100

    Init repo

--- ensembler/ensembler/env_config.py
+++ ensembler/ensembler/env_config.py
@@ -8,9 +8,9 @@ def create_env_config():
     config = {}
     config["APP_NAME"] = os.environ.get("APP_NAME", "demo")
     config["AMQ_USER"] = os.environ.get("AMQ_USER", "admin")
-    config["AMQ_PASSWORD"] = os.environ.get("AMQ_PASSWORD", "admin")
-    config["AMQ_HOST"] = os.environ.get("AMQ_HOST", "localhost")
-    config["AMQ_PORT_BROKER"] = os.environ.get("AMQ_PORT_BROKER", "61613")
+    config["AMQ_PASSWORD"] = os.environ.get("AMQ_PASSWORD", "nebulous")
+    config["AMQ_HOST"] = os.environ.get("AMQ_HOST", "nebulous-activemq")
+    config["AMQ_PORT_BROKER"] = os.environ.get("AMQ_PORT_BROKER", "61616")
     config["START_TOPIC"] = "start_ensembler"
     config["TZ"] = os.environ.get("TIME_ZONE", "Europe/Vienna")
     config["LOGING_FILE_NAME"] = os.environ.get("LOGING_FILE_NAME", "ensembler")
