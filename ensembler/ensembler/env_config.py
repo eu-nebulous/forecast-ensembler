a1464f66689bafe2f3b8013da43fce6cf340dc2d
jmarchel7bulls <jmarchel@7bulls.com>
2025-01-30 14:14:01 +0100

    Fix wrong config env

--- ensembler/ensembler/env_config.py
+++ ensembler/ensembler/env_config.py
@@ -9,7 +9,7 @@ def create_env_config():
     config["APP_NAME"] = os.environ.get("APP_NAME", "demo")
     config["AMQ_USER"] = os.environ.get("AMQ_USER", "admin")
     config["AMQ_PASSWORD"] = os.environ.get("AMQ_PASSWORD", "nebulous")
-    config["AMQ_HOST"] = os.environ.get("AMQ_HOST", "localhost")
+    config["AMQ_HOST"] = os.environ.get("AMQ_HOST", "nebulous-activemq")
     config["AMQ_PORT_BROKER"] = os.environ.get("AMQ_PORT_BROKER", "5672")
     config["START_TOPIC"] = "eu.nebulouscloud.forecasting.start_ensembling"
     config["ENSEMBLE_TOPIC"] = "eu.nebulouscloud.forecasting.ensemble"
